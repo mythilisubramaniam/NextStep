ab3f155ad033a4d53667b79c04bc152e97346436
mythilisubramaniam <mythilisubramani13@gmail.com>
2026-02-08 15:53:12 +0530

    mongoosh

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
 
+
 // Admin dashboard
 router.get("/dashboard", adminController.loadDashboard);
 
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
+require('dotenv').config();
+const connectDB = require('./backend/config/db');
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,13 +15,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
 const userRouter = require("./router/userRouter");
 const adminRouter = require("./router/adminRouter");
 
 app.use("/", userRouter);
 app.use("/admin", adminRouter);
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
